c7a158ab863bcc3c0ceb9eb001b73af277d756da
Istvan SZALAI <szalai972@gmail.com>
2016-10-06 23:35:49 -0400

    [ADD] tp1.c with needed library and basic code structure

---
+++ tp1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+// ---------- //
+// Constantes //
+// ---------- //
+
+#define CARACTERE_VIDE '-'
+#define HAUTEUR_MAX 15
+#define LARGEUR_MAX 20
+
+
+int main(int argc, char *argv[]) {
+
+}
+
