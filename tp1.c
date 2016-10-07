1ba7e061f5feafd0fe33c60f5e8ae39a185be54c
Istvan SZALAI <szalai972@gmail.com>
2016-10-06 23:42:03 -0400

    [task] There must always have 3 arguments when running.

--- tp1.c
+++ tp1.c
@@ -12,8 +12,15 @@
 #define HAUTEUR_MAX 15
 #define LARGEUR_MAX 20
 
+void test_argc(int argc);
 
 int main(int argc, char *argv[]) {
-
+    test_argc(argc);
 }
+
+void test_argc(int argc){
+    if (argc < 4){
+        printf("Nombre d'arguments invalides: il en faut 3\n");
+    }
+}   
 
