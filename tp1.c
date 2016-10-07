4dc096456f4ccc37230b05dffe8505179bba0baf
Istvan SZALAI <szalai972@gmail.com>
2016-10-06 23:48:01 -0400

    [task]Both characters must be distinct, so that the display is readable.

--- tp1.c
+++ tp1.c
@@ -13,14 +13,22 @@
 #define LARGEUR_MAX 20
 
 void test_argc(int argc);
+void test_single_args(char *str1, char *str2);
 
 int main(int argc, char *argv[]) {
     test_argc(argc);
+    test_single_args(argv[1],argv[2]);
 }
 
 void test_argc(int argc){
     if (argc < 4){
         printf("Nombre d'arguments invalides: il en faut 3\n");
     }
-}   
+}
+
+void test_single_args(char *str1, char *str2){
+    if (strcmp(str1,str2) == 0){
+        printf("Les codes doivent etre distincts\n");
+    }
+}
 
