3daedfb93f02960e72204cf14453d376337d04e8
Istvan SZALAI <szalai972@gmail.com>
2016-10-07 20:53:35 -0400

    Huge Refactoring in order to use data structures:     * Change function signatures [again \(*_*)/...]     * data structure for mountain and ArrayHeights     * All validation test
     Changes to be committed:
    	modified:   tp1.c

--- tp1.c
+++ tp1.c
@@ -5,66 +5,121 @@
 #include <stdbool.h>
 
 // ---------- //
-// Constantes //
+// Constants  //
 // ---------- //
 
-#define CARACTERE_VIDE '-'
-#define HAUTEUR_MAX 15
-#define LARGEUR_MAX 20
+#define EMPTY_CHAR '-'
+#define HEIGHT_MAX 15
+#define WIDTH_MAX 20
 
-void test_argc(int argc);
-void test_single_args(char *str1, char *str2);
-void test_args_len(char *str);
-void test_width(int len);
-int slice_string(char **array_str,char *str);
+// --------------------- //
+//    DATA STRUCTURES    //
+// --------------------- //
+
+typedef struct ArrayHeights {
+    unsigned int width;
+    int content[WIDTH_MAX + 1];
+} ArrayHeights;
+
+typedef struct Mountain {
+    unsigned int HEIGHT;
+    unsigned int WIDTH;
+    char content[WIDTH_MAX][HEIGHT_MAX];
+} Mountain;
+
+// ---------- //
+// Prototypes //
+// ---------- //
+
+void checkArgc(int argc);
+void isWaterAndDirtDifferent(char *dirt, char *water);
+void isSingleArg(char *s);
+void isArrayHeightsValid(ArrayHeights array_heights);
+int toString(char []);
+void createArrayHeights(ArrayHeights array_heights,char *str);
 
 int main(int argc, char *argv[]) {
-    test_argc(argc);
-    test_args_len(argv[1]);
-    test_args_len(argv[2]);
-    test_single_args(argv[1],argv[2]);
+    checkArgc(argc);
+    isWaterAndDirtDifferent(argv[1],argv[2]);
+    isSingleArg(argv[1]);
+    isSingleArg(argv[2]);
 
-    char *array[strlen(argv[argc-1]-1)];
-    int len = slice_string(array, argv[argc-1]);
-    test_width(len);
+    ArrayHeights array_heights;
+
+    createArrayHeights(array_heights, argv[argc-1]);
+    //isArrayHeightsValid(array_heights);
 }
 
-void test_argc(int argc){
+void checkArgc(int argc){
     if (argc < 4){
         printf("Nombre d'arguments invalides: il en faut 3\n");
         exit(1);
     }
 }
 
-void test_single_args(char *str1, char *str2){
-    if (strcmp(str1,str2) == 0){
+void isWaterAndDirtDifferent(char *dirt, char *water){
+    if (strcmp(dirt,water) == 0){
         printf("Les codes doivent etre distincts\n");
         exit(1);
     }
 }
 
-void test_args_len(char *str){
-    if (strlen(str) != 1){
-        printf("Code %s invalide: il doit etre un caractere unique\n",str);
+void isSingleArg(char *s){
+    if (strlen(s) != 1){
+        printf("Code %s invalide: il doit etre un caractere unique\n",s);
         exit(1);
     }
 }
-void test_width(int len){
-    if (len >  LARGEUR_MAX){
-        printf("Largeur invalide: le nombre de hauteurs doit etre entre 1 et 20\n");
-        exit(1);
-    }
+void isArrayHeightsValid(ArrayHeights array_heights){
+    unsigned int i;
+    for (i = 0; i < array_heights.width; i++){
+        printf("%d\n",array_heights.content[i]);
+        }
 }
 
-int slice_string(char **array_str,char *str){
+int toString(char a[]) {
+  int c, sign, offset, n;
+ 
+  if (a[0] == '-') {
+    sign = -1;
+  }
+ 
+  if (sign == -1) {
+    offset = 1;
+  }
+  else {
+    offset = 0;
+  }
+ 
+  n = 0;
+ 
+  for (c = offset; a[c] != '\0'; c++) {
+    n = n * 10 + a[c] - '0';
+  }
+ 
+  if (sign == -1) {
+    n = -n;
+  }
+ 
+  return n;
+}
+
+
+void createArrayHeights(ArrayHeights array_heights,char *str){
     const char s[2] = ",";
     char *token;
     int index = 0;
 
     token = strtok(str,s);
     while (token != NULL){
-        array_str[index++] = token;
+        if (toString(token) < 0 || toString(token) > 15){
+            printf("Hauteur invalide: la hauteur doit etre un nombre entre 0 et %d\n",HEIGHT_MAX);\
+            exit(1);
+        }
+        array_heights.content[index] = toString(token);
+        printf("%d\n",array_heights.content[index]);
+        index++;
         token = strtok(NULL,s);
     }
-    return index;
+    array_heights.width = index;
 }
